2fc78111fc793a83a95fa5189beb430347ba215d
davidpovarsky <davidpovarski1@gmail.com>
2026-09-10 01:52:52 +0300

    Fix ShowInAppSearchResultsIntent conformance

--- Sources/App/Intents/SefariaShowInAppSearchResultsIntent.swift
+++ Sources/App/Intents/SefariaShowInAppSearchResultsIntent.swift
@@ -1,9 +1,10 @@
 import AppIntents
 
 struct SefariaShowInAppSearchResultsIntent: ShowInAppSearchResultsIntent {
-    static var searchScopes: [StringSearchScope] = [.general]
+    static let title: LocalizedStringResource = "Search Sefaria"
+    static let searchScopes: [StringSearchScope] = [.general]
 
-    var criteria: StringSearchCriteria
+    @Parameter var criteria: StringSearchCriteria
 
     func perform() async throws -> some IntentResult {
         let query = criteria.term
